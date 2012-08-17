c4e48aa8d8bc7490f4bbfde24ff2aa503ed0465f
xatierlike <xatierlike@xatierlike-NB.(none)>
2012-08-18 05:58:49 +0800

    restore my progress
        refacting the codes on bus after COSCUP

---
+++ hw/stm32_gpio.c
@@ -0,0 +1,331 @@
+#include "sysbus.h"
+#include "exec-memory.h"
+
+#define NB_PIN 64
+
+typedef struct {
+    SysBusDevice busdev;
+
+    /* Registres GPIO (Reference Manual p119 */
+    uint32_t mode; /* Mode */
+    uint16_t otype; /* Output type */
+    uint32_t ospeed; /* Output speed */
+    uint32_t pupd; /* Pull-up/Pull-down */
+    uint16_t ind; /* Input data */
+    uint16_t outd; /* Output data register */
+    uint16_t outd_old; /* Output data register */
+    uint32_t bsr; /* Bit set/reset */
+    uint32_t lck; /* Lock */
+    uint32_t afrl; /* Alternate function low */
+    uint32_t afrh; /* Alternate function high */
+    
+    qemu_irq irq_out[NB_PIN];
+    unsigned char id;
+    MemoryRegion iomem;
+} stm32_gpio_state;
+
+
+static const VMStateDescription vmstate_stm32_gpio = {
+    .name       = "stm32_gpio",
+    .version_id = 1,
+    .minimum_version_id = 1,
+    .fields = (VMStateField[])
+    {
+        VMSTATE_UINT32(mode, stm32_gpio_state),
+        VMSTATE_UINT16(otype, stm32_gpio_state),
+        VMSTATE_UINT32(ospeed, stm32_gpio_state),
+        VMSTATE_UINT32(pupd, stm32_gpio_state),
+        VMSTATE_UINT16(ind, stm32_gpio_state),
+        VMSTATE_UINT16(outd, stm32_gpio_state),
+        VMSTATE_UINT16(outd_old, stm32_gpio_state),
+        VMSTATE_UINT32(bsr, stm32_gpio_state),
+        VMSTATE_UINT32(lck, stm32_gpio_state),
+        VMSTATE_UINT32(afrl, stm32_gpio_state),
+        VMSTATE_UINT32(afrh, stm32_gpio_state),
+        VMSTATE_END_OF_LIST()
+    }
+};
+
+
+static void stm32_gpio_update(stm32_gpio_state *s)
+{
+    uint16_t changed;
+    uint16_t mask;
+    int i;
+    
+    changed = s->outd_old ^ s->outd; //XOR: Tous les bits à 1 seront les bits changés
+    if (!changed)
+        return;
+
+    s->outd_old = s->outd;
+    for (i = 0; i < 8; i++) {
+        mask = 1 << i;
+        if (changed & mask) {
+            //Conditions de changement
+            //--Mode register != 00
+            uint32_t modeMask = (1 << (i*2)) | (1 << ((i*2) + 1));
+            if((s->mode & modeMask) != 0) {
+                // send an IRQ to the device connected to the GPIO pin
+                //FIXME: the transmition to the pin is direct when it should occur at the next tick of the RCC
+                qemu_set_irq(s->irq_out[i], (s->outd & mask) != 0);
+            }
+        }
+    }
+}
+
+
+static uint64_t stm32_gpio_read(void *opaque, target_phys_addr_t offset, unsigned size)
+{
+    stm32_gpio_state *s = (stm32_gpio_state *) opaque;
+
+    switch (offset) {
+        case 0x00: /* Mode */
+            return s->mode;
+        case 0x04: /* oType */
+            return s->otype;
+        case 0x08: /* oSpeed */
+            return s->ospeed;
+        case 0x0C: /* Pull-up / Pull-down */
+            return s->pupd;
+        case 0x10: /* Input data register */
+            return s->ind;
+        case 0x14: /* Output data */
+            return s->outd;
+        case 0x18: /* BSR */
+            return 0x0; //Write only
+        case 0x1C: /* lock */
+            return 0x0; //Non implémenté
+        case 0x20: /* AFRL */
+            return s->afrl;
+        case 0x24: /* AFRH */
+            return s->afrh;
+        default:
+            hw_error("stm32_gpio_read: Bad offset %x\n", (int) offset);
+            return 0;
+    }
+}
+
+
+static void stm32_gpio_write(void *opaque, target_phys_addr_t offset, 
+                                        uint64_t value, unsigned size)
+{
+    stm32_gpio_state *s = (stm32_gpio_state *) opaque;
+    int i;
+    uint16_t low = (uint16_t)value;
+    uint16_t high = (uint16_t)value >> 16;
+    
+    switch (offset) {
+        case 0x00: /* Mode */
+            s->mode = value;
+            break;
+        case 0x04: /* oType */
+            s->otype = value;
+            break;
+        case 0x08: /* oSpeed */
+            s->ospeed = value;
+            break;
+        case 0x0C: /* Pull-up / Pull-down */
+            s->pupd = value;
+            break;
+        case 0x10: /* Input data register */
+            //Read only
+            break;
+        case 0x14: /* Output data */
+            s->outd = value;
+            stm32_gpio_update(s);
+            break;
+            
+        case 0x18: /* BSR */
+            //set = low
+            //reset = high
+            for(i=0; i<16; i++) {
+                int mask = 1 << i;
+                if((high & mask) != 0) { //Si bit reset[i]
+                    s->outd &= ~(1 << i); //Mise à 0
+                }
+                if((low & mask) != 0) { //Si bit reset[i]
+                    s->outd |= (1 << i); //Mise à 1
+                }
+            }
+            stm32_gpio_update(s);
+            break;
+            
+        case 0x1C: /* lock */
+            //FIXME: Non implémenté
+            break;
+        case 0x20: /* AFRL */
+            s->afrl = value;
+            break;
+        case 0x24: /* AFRH */
+            s->afrh = value;
+            break;
+        default:
+            hw_error("stm32_gpio_write: Bad offset %x\n", (int) offset);
+    }
+}
+
+
+
+static void stm32_gpio_reset(stm32_gpio_state *s)
+{
+    switch (s->id) {
+        case 'A':
+            s->mode = 0xA8000000;
+            s->pupd = 0x64000000;
+            s->ospeed = 0x00000000;
+            break;
+        case 'B':
+            s->mode = 0x00000280;
+            s->pupd = 0x00000100;
+            s->ospeed = 0x000000C0;
+            break;
+        default:
+            s->mode = 0x00000000;
+            s->pupd = 0x00000000;
+            s->ospeed = 0x00000000;
+    }
+    
+    //Valeur commune
+    s->ind =    0x00000000;
+    s->otype =  0x00000000;
+    s->outd =   0x00000000;
+    s->outd_old = s->outd;
+    s->bsr =    0x00000000;
+    s->lck =    0x00000000;
+    s->afrh =   0x00000000;
+    s->afrl =   0x00000000;
+
+}
+
+/*
+ * Appelé quand une entrée de GPIO reçois une IT
+ * called when an input GPIO receive an IT
+ */
+static void stm32_gpio_in_recv(void * opaque, int numPin, int level)
+{
+    assert(numPin>=0 && numPin<NB_PIN);
+    stm32_gpio_state *s = (stm32_gpio_state *) opaque;
+    
+    
+    uint32_t mask = (1 << (numPin*2)) | (1 << ((numPin*2) + 1));
+    //check if the pin is confihured as input
+    //--Pull-up Pull-Down -> must be different trom 00
+    if((s->pupd & mask) == 0) {return;} 
+    //--Mode register -> 00
+    if((s->mode & mask) != 0) {return;} 
+    
+    //Writing to the register input data register
+    printf("GPIO_in[%d]->%d\n", numPin, level);
+    if(level) {
+        s->ind |= (1 << numPin); // last => 1
+    } else {
+        s->ind &= ~(1 << numPin); //Mise à 0
+    }
+}
+
+
+
+static const MemoryRegionOps gpio_ops = {
+    .read  = stm32_gpio_read,
+    .write = stm32_gpio_write,
+    .endianness = DEVICE_NATIVE_ENDIAN,
+};
+
+
+
+static int stm32_gpio_init(SysBusDevice *dev, const unsigned char id)
+{
+    stm32_gpio_state *s = FROM_SYSBUS(stm32_gpio_state, dev);
+    s->id = id;
+    
+    // initialisation of the memory range
+    memory_region_init_io(&s->iomem, &gpio_ops, s, "gpio", 0x17FF);
+    sysbus_init_mmio(dev, &s->iomem);
+    
+    // initial the pins
+    qdev_init_gpio_in(&dev->qdev, stm32_gpio_in_recv, NB_PIN);
+    qdev_init_gpio_out(&dev->qdev, s->irq_out, NB_PIN);
+
+    stm32_gpio_reset(s);
+    vmstate_register(&dev->qdev, -1, &vmstate_stm32_gpio, s);
+
+    return 0;
+}
+
+
+
+static int stm32_gpio_init_A(SysBusDevice *dev)
+{
+    return stm32_gpio_init(dev, 'A');
+}
+
+
+
+static int stm32_gpio_init_B(SysBusDevice *dev)
+{
+    return stm32_gpio_init(dev, 'B');
+}
+
+
+
+static Property stm32_gpio_sysbus_properties[] = {
+    //XXX: DEFINE_PROP_CHR("chardev", NULL, chr),
+    DEFINE_PROP_END_OF_LIST(),
+};
+
+
+
+static void stm32_gpio_class_init_A (ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+    SysBusDeviceClass *k = SYS_BUS_DEVICE_CLASS(klass);
+
+    k->init   = stm32_gpio_init_A;
+    dc->desc  = "STM32 GPIO A";
+    dc->vmsd  = &vmstate_stm32_gpio;
+    dc->props = stm32_gpio_sysbus_properties;
+
+}
+
+
+
+static void stm32_gpio_class_init_B (ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+    SysBusDeviceClass *k = SYS_BUS_DEVICE_CLASS(klass);
+
+    k->init   = stm32_gpio_init_B;
+    dc->desc  = "STM32 GPIO B";
+    dc->vmsd  = &vmstate_stm32_gpio;
+    dc->props = stm32_gpio_sysbus_properties;
+
+}
+
+
+
+static TypeInfo stm32_gpioA_info = {
+    .name       = "stm32_gpio_A",
+    .parent     = TYPE_SYS_BUS_DEVICE,
+    .instance_size = sizeof (stm32_gpio_state),
+    .class_init = stm32_gpio_class_init_A,
+};
+
+
+
+static TypeInfo stm32_gpioB_info = {
+    .name       = "stm32_gpio_B",
+    .parent     = TYPE_SYS_BUS_DEVICE,
+    .instance_size = sizeof (stm32_gpio_state),
+    .class_init = stm32_gpio_class_init_B,
+};
+
+
+
+static void stm32_gpio_register_devices(void) {
+    type_register_static(&stm32_gpioA_info);
+    type_register_static(&stm32_gpioB_info);
+}
+
+
+
+type_init(stm32_gpio_register_devices)
---
+++ hw/stm32_led.c
@@ -0,0 +1,141 @@
+
+#include "sysbus.h"
+
+typedef struct {
+    SysBusDevice busdev;
+    uint8_t      ledState;
+    CharDriverState *chr;
+} stm32_led_state;
+
+
+
+static const VMStateDescription vmstate_stm32_led = {
+    .name       = "stm32_led",
+    .version_id = 1,
+    .minimum_version_id = 1,
+    .fields = (VMStateField[])
+    {
+        VMSTATE_UINT8(ledState, stm32_led_state),
+        VMSTATE_END_OF_LIST()
+    }
+};
+
+
+
+static void stm32_led_reset (stm32_led_state *s)
+{
+    s->ledState = 0;
+}
+
+/*
+ * Appelé quand une entrée de led reçoit une IT
+ */
+static void stm32_led_recvirq (void *opaque, int num_pin, int level)
+{
+    /* the LED change state */
+    stm32_led_state *s = (stm32_led_state *) opaque;
+    
+    if (s->chr) {
+        uint8_t buffer = (uint8_t)level;
+        qemu_chr_fe_write(s->chr, &buffer, 1);
+    }
+}
+
+
+
+static int stm32_led_init (SysBusDevice *dev, const char *id)
+{
+    stm32_led_state *s = FROM_SYSBUS(stm32_led_state, dev);
+    
+    /* initialize the output pins */
+    qdev_init_gpio_in(&dev->qdev, stm32_led_recvirq, 1);
+
+    /* initialize the char device */
+    s->chr = qemu_chr_find(id);
+    
+    stm32_led_reset(s);
+    vmstate_register(&dev->qdev, -1, &vmstate_stm32_led, s);
+
+    return 0;
+}
+
+ 
+
+static int stm32_led_init_Blue (SysBusDevice *dev)
+{
+    return stm32_led_init(dev, "led_blue");
+}
+
+
+
+static int stm32_led_init_Green(SysBusDevice *dev)
+{
+    return stm32_led_init(dev, "led_green");
+}
+
+
+
+static Property stm32_led_blue_sysbus_properties[] = {
+    DEFINE_PROP_END_OF_LIST(),
+};
+
+
+
+static Property stm32_led_green_sysbus_properties[] = {
+    DEFINE_PROP_END_OF_LIST(),
+};
+
+
+
+static void stm32_led_blue_class_init (ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+    SysBusDeviceClass *k = SYS_BUS_DEVICE_CLASS(klass);
+
+    k->init   = stm32_led_init_Blue;
+    dc->desc  = "STM32 LED Blue";
+    dc->vmsd  = &vmstate_stm32_led,
+    dc->props = stm32_led_blue_sysbus_properties;
+}
+
+
+
+static void stm32_led_green_class_init (ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+    SysBusDeviceClass *k = SYS_BUS_DEVICE_CLASS(klass);
+
+    k->init   = stm32_led_init_Green;
+    dc->desc  = "STM32 LED Green";
+    dc->vmsd  = &vmstate_stm32_led,
+    dc->props = stm32_led_green_sysbus_properties;
+}
+
+
+
+static TypeInfo stm32_led_info_blue = {
+    .name       = "stm32_led_blue",
+    .parent     = TYPE_SYS_BUS_DEVICE,
+    .instance_size = sizeof (stm32_led_state),
+    .class_init = stm32_led_blue_class_init,
+};
+
+
+
+static TypeInfo stm32_led_info_green = {
+    .name       = "stm32_led_green",
+    .parent     = TYPE_SYS_BUS_DEVICE,
+    .instance_size = sizeof (stm32_led_state),
+    .class_init = stm32_led_green_class_init,
+};
+
+
+
+static void stm32_led_register_devices(void) {
+    type_register_static(&stm32_led_info_blue);
+    type_register_static(&stm32_led_info_green);
+}
+
+
+
+type_init(stm32_led_register_devices)
